6184e3573d7aa2f3bf9e53fb453dc7ff2ce44605
henriquegomes6 <henriquegomes6@hotmail.com>
2019-02-03 21:33:27 -0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,44 @@
-const webpack = require('webpack');
-const ExtracTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + 'public',
+        path: __dirname + '/public',
         filename: './app.js'
-    }, devServer: {
+    },
+    devServer: {
         port: 8080,
         contentBase: './public',
-    }, resolve: {
+    },
+    resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-            bootstrap: 'modules/admin-lte/plugins/js/bootstrap.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
-    }, plugins: [
-        new webpack.ProgressPlugin({
-            $: 'query',
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
-        new ExtracTextPlugin('app.css')
+        new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-            test: /js[x]?$/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                presets: ['es2012', 'react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
-            loader: ExtracTextPlugin.extract('style-loader', 'css-loader')
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
             loader: 'file'
