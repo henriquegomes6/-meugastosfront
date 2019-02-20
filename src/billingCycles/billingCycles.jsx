95a609397c70ef27350b434d1cda540aa71af335
henriquegomes6 <henriquegomes6@hotmail.com>
2019-02-19 21:30:11 -0300

    add tabs

--- src/billingCycles/billingCycles.jsx
+++ src/billingCycles/billingCycles.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ContentHeader from '../common/template/contentHeader';
+import Content from '../common/template/content';
+import Tabs from '../common/tab/tabs';
+import TabsContent from '../common/tab/tabsContent';
+import TabsHeader from '../common/tab/tabsHeader';
 
+class BillingCycles extends Component {
 
-export default props => (
-    <div>
-        Pagamentos
-    </div>
-);
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div >
+        );
+    }
+}
+
+export default BillingCycles;
